2515a65848b0e2854671157a914deeed00eb1797
SandeepKomal <sandeepkomalp@gmail.com>
2023-12-07 15:22:33 +0530

    Update PackersMoversController.java

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -30,7 +30,7 @@ public class PackersMoversController {
         
         
         @GetMapping("/ping")
-        public String ping() {  return "Hi, this is sandeep komal" ;
+        public String ping() {  return "Hi, this is sandeep komal pothu" ;
         			
         }
         
